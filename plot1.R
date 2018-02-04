50793b60a2592e338d4e930e7668348b80b1109b
hprattipati <33036216+hprattipati@users.noreply.github.com>
2018-02-04 17:59:56 -0500

    Add files via upload

---
+++ plot1.R
@@ -0,0 +1,6 @@
+readUCIdata=read.table("household_power_consumption.txt",header=TRUE,sep=";",na.strings="?")
+readUCIdata$Date=as.Date(readUCIdata$Date,"%d/%m/%Y")
+finaldt=subset(readUCIdata,Date>=as.Date("2007-02-01") & Date <=as.Date("2007-02-02"))
+png(filename="plot1.png",width=480,height=480, units="px")
+hist(finaldt$Global_active_power,col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
+dev.off()
